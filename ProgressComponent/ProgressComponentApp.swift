ed1a6b41cbf609aef79f5513e71fbe878ab9b920
Debartha Chakraborty <deba32cty@gmail.com>
2023-10-29 11:19:51 +0530

    Initial Commit

---
+++ ProgressComponent/ProgressComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressComponentApp.swift
+//  ProgressComponent
+//
+//  Created by Debartha Chakraborty on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
